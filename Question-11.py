1925fc6a73d4831a04d0c3413da463f542ea6aac
2pro4younoob <80782960+2pro4younoob@users.noreply.github.com>
2022-10-27 12:39:34 +0530

    I have solved the problem by Recursion. Space would be O(N) and time complexity is O(2^N)

---
+++ Question-11.py
@@ -0,0 +1,12 @@
+def TowerOfHanoi(n, from_rod, to_rod, aux_rod):
+	if n == 0:
+		return
+	TowerOfHanoi(n-1, from_rod, aux_rod, to_rod)
+	print("Move disk", n, "from rod", from_rod, "to rod", to_rod)
+	TowerOfHanoi(n-1, aux_rod, to_rod, from_rod)
+
+#we have the driver code below this 
+N = 3
+
+# A, C, B are the name of rods
+TowerOfHanoi(N, 'A', 'C', 'B')
